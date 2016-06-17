304cdb52fae3bc83d89d70326c75ad98fbffade1
bkim28964 <bhkim7000@gmail.com>
2016-06-17 13:33:25 -0500

    Forgot-password code cleanup
    Cleaned this as well

--- forgot-password.php
+++ forgot-password.php
@@ -1,74 +1,15 @@
 <?php
-require_once('admin/includes/configurations.php');
 $body_id = 'forgotbody';
-require_once('logincheck.php');
-require_once('admin/module/classStudent.php');
 
-if( isset( $_POST['fp_submit'] ) )
-{
-		
-    $rules  = array(); 
-
-    $rules[]  = "required,fp_email,Please enter email address.";
-    $rules[]  = "valid_email,fp_email,Please enter a valid email address.";
-    
-    $errors   = validateFields($_POST, $rules);
-
-    if (!empty($errors)){  
-        $fields = $_POST;  
-    }else{
-      $studentRecord = $StudentObj->getStudentByEmail( $CommanObj->inscrape($_POST['fp_email']));
-      if($studentRecord){
-         $hash = md5(uniqid($_POST['fp_email'], true));
-         if($StudentObj->updateStudentHash($studentRecord['id'], $hash)){
-            $reset_link = $baseSite . '/reset-password.php?rp=' . $hash;
-            $to = $_POST['fp_email'];
-            $subject = 'Studybuddy Password Reset';       
-            $headers = "MIME-Version: 1.0" . "\r\n";
-            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-            
-            $message = "Hi,<br />Here is the link to reset your password: <a href='$reset_link'>$reset_link</a>.<br />Thank you.";
-            
-            $mail_sent = mail( $to, $subject, $message, $headers );
-            
-            if( $mail_sent )
-            {
-              $success_msg = 'Password reset link has been sent on your email address.'; 
-            }
-            else
-            {
-              $errors[] = 'There was some problem sending the password reset link on this email.';     
-            }
-         }else{
-            $errors[] = 'There was some problem generating the password reset link.';   
-         } 
-          
-      }else{
-        $errors[]   =  "Your email address is not currently associated with an account.";     
-        $fields = $_POST;
-      }
-    }
-		
-}//submit check
 ?>
 <?php require_once('header.php'); ?>
   <div class="white_bg padding-page">
     <div class="container">
     <div class="col-xs-12 col-md-5 col-sm-7 col-centered">
     	
-          <?php
-          	if( isset( $success_msg ) )
-			{
-				echo '<p class="success_msg">'.$success_msg.'</p>';
-			}
-          if( isset($errors) && count($errors) >0 ){
-            foreach($errors as $key=>$val){
-        ?>
-            <p class="error_msg"><?php echo $val;?></p>
-        <?php
-            }
-          }
-		  ?>
+         
+            <p class="error_msg"></p>
+       
     
         <div class="login_box">
           <div class="title">Enter your email</div>
